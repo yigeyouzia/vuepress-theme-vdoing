4582c31db40482be8393fafb34139daa5d0b33e6
cyt <chenyouting22@163.com>
2024-12-18 16:37:03 +0800

    dev

---
+++ deploy copy 2.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+# 确保脚本抛出遇到的错误
+set -e
+npm run build # 生成静态文件
+cd docs/.vuepress/dist # 进入生成的文件夹
+
+# deploy to github
+echo 'blog.youcyt.ltd' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:yigeyouzia/vuepress-theme-vdoing.git
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://yigeyouzia:${GITHUB_TOKEN}@github.com/yigeyouzia/vuepress-theme-vdoing.git
+  git config --global user.name "cyt"
+  git config --global user.email "1793402316@qq.com"
+fi
+git init
+git add -A
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
+
+# deploy to coding
+echo 'press.youcyt.ltd' > CNAME  # 自定义域名
+if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+  codingUrl=git@e.coding.net:youcyt/youcyt/vuepress.git
+else
+  codingUrl=https://youcyt:${CODING_TOKEN}@e.coding.net:youcyt/youcyt/vuepress.git
+fi
+git add -A
+git commit -m "${msg}"
+git push -f $codingUrl master # 推送到coding
+
+cd -
+rm -rf docs/.vuepress/dist
--- deploy.sh
+++ deploy.sh
@@ -1,35 +1,24 @@
 #!/usr/bin/env sh
+
 # 确保脚本抛出遇到的错误
 set -e
-npm run build # 生成静态文件
-cd docs/.vuepress/dist # 进入生成的文件夹
 
-# deploy to github
-echo 'blog.youcyt.ltd' > CNAME
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:yigeyouzia/vuepress-theme-vdoing.git
-else
-  msg='来自github action的自动部署'
-  githubUrl=https://yigeyouzia:${GITHUB_TOKEN}@github.com/yigeyouzia/vuepress-theme-vdoing.git
-  git config --global user.name "cyt"
-  git config --global user.email "1793402316@qq.com"
-fi
+
+push_addr=git@github.com:yigeyouzia/vuepress-theme-vdoing.git # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+commit_info=`git describe --all --always --long`
+dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
+push_branch=gh-pages # 推送的分支
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd $dist_path
+
 git init
 git add -A
-git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github
-
-# deploy to coding
-echo 'press.youcyt.ltd' > CNAME  # 自定义域名
-if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:youcyt/youcyt/vuepress.git
-else
-  codingUrl=https://youcyt:${CODING_TOKEN}@e.coding.net:youcyt/youcyt/vuepress.git
-fi
-git add -A
-git commit -m "${msg}"
-git push -f $codingUrl master # 推送到coding
+git commit -m "deploy, $commit_info"
+git push -f $push_addr HEAD:$push_branch
 
 cd -
-rm -rf docs/.vuepress/dist
+rm -rf $dist_path
